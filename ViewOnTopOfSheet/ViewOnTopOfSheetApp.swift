0264918562cbf18bfd4c0169398e23ebe57816d1
dqhieu <dqhieu13@gmail.com>
2022-02-18 19:40:05 +0700

    Initial Commit

---
+++ ViewOnTopOfSheet/ViewOnTopOfSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewOnTopOfSheetApp.swift
+//  ViewOnTopOfSheet
+//
+//  Created by Dinh Quang Hieu on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewOnTopOfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
