9733b5390b45f6ccd7e00881bcb249d0181a7c5d
dqhieu <dqhieu13@gmail.com>
2022-02-18 19:58:52 +0700

    first commit

--- ViewOnTopOfSheet/ContentView.swift
+++ ViewOnTopOfSheet/ContentView.swift
@@ -6,16 +6,72 @@
 //
 
 import SwiftUI
+import UIKit
+
+var overlayWindow: UIWindow?
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State var isPresenting = false
+  
+  var body: some View {
+    ZStack {
+      Button {
+        isPresenting.toggle()
+      } label: {
+        Text("Button")
+      }
+      .sheet(isPresented: $isPresenting) {
+        Text("Hello world")
+      }
     }
+    .onAppear {
+      showOverlayView()
+    }
+  }
+  
+  func showOverlayView() {
+    if let currentWindowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+      overlayWindow = UIHigherWindow(windowScene: currentWindowScene)
+    }
+    overlayWindow?.windowLevel = UIWindow.Level.alert + 1
+    let hostVC = UIHostingController(rootView: OverlayView())
+    hostVC.view.backgroundColor = .clear
+    overlayWindow?.rootViewController = hostVC
+    overlayWindow?.backgroundColor = UIColor.clear
+    overlayWindow?.makeKeyAndVisible()
+  }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct OverlayView: View {
+  var body: some View {
+    VStack {
+      Spacer()
+      RoundedRectangle(cornerRadius: 20)
+        .fill(.red)
+        .frame(width: 100, height: 100)
     }
+  }
+}
+
+class UIHigherWindow: UIWindow {
+  
+  override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+    
+    let hitView = super.hitTest(point, with: event)
+    
+    if hitView!.isKind(of: UIView.self) {
+      
+      return nil
+    }
+    
+    return hitView
+  }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
 }
